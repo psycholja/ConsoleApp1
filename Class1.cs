2ea83297de1cd9f1652c025739f0cdea4eab953a
psycholja <kolosowskaja@gmail.com>
2020-10-03 20:00:56 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Box
+    {
+        int width;
+        int higth;
+        int length;
+
+        public void setWidth(int param)
+        {
+            this.width = param;
+
+        }
+        public void setHigth(int param)
+        {
+            this.higth = param;
+
+        }
+        public void setLength(int param)
+        {
+            this.length = param;
+
+        }
+        public int getVolume()
+        {
+            int c;
+            c = this.width * this.higth * this.length;
+            return c;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Try again");
+            Box myBox = new Box();
+            myBox.setHigth(5);
+            myBox.setLength(5);
+            myBox.setWidth(5);
+            Console.WriteLine("Volume is {0} {1}", myBox.getVolume(), "m3");
+
+        }
+    }
+}
